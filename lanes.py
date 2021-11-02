ed30a7727dfde542744534f78dcc66ed56b48aeb
Obierefu Finbarr Chukwuka <57522378+finbarr91@users.noreply.github.com>
2021-11-02 16:00:15 -0700

    Region of interest calculation

--- lanes.py
+++ lanes.py
@@ -1,1 +1,26 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def canny(image):
+    gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    canny = cv2.Canny(blur, 50, 150)
+    return canny
+
+def region_of_interest(image):
+    height = image.shape[0]
+    polygons = np.array([[(200,height),(1100,height),(550,250)]])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask,polygons,255)
+    masked_image = cv2.bitwise_and(image,mask) # Computing the bitwise & of both immages in the theory section, takes the bitwise & of each homologous pixel in both arrays,
+    # utimately masking the canny image to only show the region of interest traced by the polygonal contour of the mask
+    return masked_image
+
+image= cv2.imread('test_image.jpg')
+lane_image = np.copy(image)
+canny = canny(lane_image)
+cropped_image = region_of_interest(canny)
+cv2.imshow('result',cropped_image)
+cv2.waitKey(0)
 
